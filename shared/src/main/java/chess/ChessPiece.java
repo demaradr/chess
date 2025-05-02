6c86443584597e643a2f03cf25f5a65c25cc1505
Adriano Demartin <adriano.demartinez@gmail.com>
2025-05-02 00:09:40 -0600

    implemented ChessPiece functions including pieceMoves

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,5 +1,7 @@
 package chess;
 
+import chess.PieceMovesCalculator.BishopMovesCalculator;
+
 import java.util.ArrayList;
 import java.util.Collection;
 
@@ -11,7 +13,12 @@ import java.util.Collection;
  */
 public class ChessPiece {
 
-    public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+    private final PieceType type;
+    private final ChessGame.TeamColor pieceColor;
+
+    public ChessPiece(ChessGame.TeamColor pieceColor, PieceType type) {
+        this.pieceColor = pieceColor;
+        this.type = type;
     }
 
     /**
@@ -30,14 +37,14 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return pieceColor;
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return type;
     }
 
     /**
@@ -48,6 +55,14 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        return new ArrayList<>();
+        return switch (type) {
+            case KING -> null;
+            case QUEEN -> null;
+            case BISHOP -> new BishopMovesCalculator().getMoves(board, myPosition);
+            case KNIGHT -> null;
+            case ROOK -> null;
+            case PAWN -> null;
+        };
     }
+
 }
