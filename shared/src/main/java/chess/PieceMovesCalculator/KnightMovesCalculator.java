13812b8bb0599eff20988ba67a71b093088d67ab
Adriano Demartin <adriano.demartinez@gmail.com>
2025-05-02 22:36:28 -0600

    created KnightMovesCalculator

---
+++ shared/src/main/java/chess/PieceMovesCalculator/KnightMovesCalculator.java
@@ -0,0 +1,36 @@
+package chess.PieceMovesCalculator;
+
+import chess.*;
+import java.util.HashSet;
+
+public class KnightMovesCalculator implements MovesCalculator {
+    @Override
+    public HashSet<ChessMove> getMoves(ChessBoard board, ChessPosition myPosition) {
+        HashSet<ChessMove> moves = new HashSet<>();
+        int[][] directions = {
+                {-2, 1}, {-2, -1},
+                {2, 1}, {2, -1},
+                {1, 2}, {-1, 2},
+                {-1, -2}, {1, -2}
+        };
+
+        for (int[] dir : directions) {
+            int row = myPosition.getRow() + dir[0];
+            int col = myPosition.getColumn() + dir[1];
+
+            // Check bounds
+            if (row >= 1 && row <= 8 && col >= 1 && col <= 8) {
+                ChessPosition newPos = new ChessPosition(row, col);
+                ChessPiece occupyingPiece = board.getPiece(newPos);
+
+                if (occupyingPiece == null) {
+                    moves.add(new ChessMove(myPosition, newPos, null));
+                } else if (occupyingPiece.getTeamColor() != board.getPiece(myPosition).getTeamColor()) {
+                    moves.add(new ChessMove(myPosition, newPos, null));
+                }
+            }
+        }
+
+        return moves;
+    }
+}
