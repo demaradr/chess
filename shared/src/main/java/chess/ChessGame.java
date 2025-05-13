9452c9f373c750ccb79912b812417221628fb209
Adriano Demartin <adriano.demartinez@gmail.com>
2025-05-12 22:56:51 -0600

    implemented isInCheckmate

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -202,7 +202,28 @@ public class ChessGame {
      * @return True if the specified team is in checkmate
      */
     public boolean isInCheckmate(TeamColor teamColor) {
-        throw new RuntimeException("Not implemented");
+        if (!isInCheck(teamColor)) {
+            return false;
+        }
+
+        ChessBoard board = getBoard();
+
+        for (int row = 1; row <= 8; row++) {
+            for (int col = 1; col <= 8; col++) {
+                ChessPosition pos = new ChessPosition(row, col);
+                ChessPiece piece = board.getPiece(pos);
+
+                if (piece != null && piece.getTeamColor() == teamColor) {
+                    Collection<ChessMove> moves = validMoves(pos);
+
+                    if (moves != null && !moves.isEmpty()) {
+                        return false;
+                    }
+                }
+
+            }
+        }
+        return true;
     }
 
     /**
