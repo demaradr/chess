941cc2784b6c36203a35138d4b804efdd973faf3
Adriano Demartin <adriano.demartinez@gmail.com>
2025-05-12 22:57:41 -0600

    implemented setBoard and getBoard

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -263,7 +263,7 @@ public class ChessGame {
      * @param board the new board to use
      */
     public void setBoard(ChessBoard board) {
-        throw new RuntimeException("Not implemented");
+        this.board = board;
     }
 
     /**
@@ -272,6 +272,6 @@ public class ChessGame {
      * @return the chessboard
      */
     public ChessBoard getBoard() {
-        throw new RuntimeException("Not implemented");
+        return board;
     }
 }
