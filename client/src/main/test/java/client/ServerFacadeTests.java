3f554db3d64feab47f958280cef29cd020244787
Adriano Demartin <adriano.demartinez@gmail.com>
2025-06-03 19:06:42 -0600

    added first tests

--- client/src/main/test/java/client/ServerFacadeTests.java
+++ client/src/main/test/java/client/ServerFacadeTests.java
@@ -1,29 +1,56 @@
 package client;
 
+import dataaccess.*;
+import model.*;
 import org.junit.jupiter.api.*;
 import server.Server;
+import static org.junit.jupiter.api.Assertions.*;
 
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ServerFacadeTests {
 
-    private static Server server;
+    private Server server;
+    private ServerFacade facade;
+
+    private AuthDAO authDAO;
+    private GameDAO gameDAO;
+    private UserDAO userDAO;
 
     @BeforeAll
-    public static void init() {
+    void startServer() throws DataAccessException {
         server = new Server();
-        var port = server.run(0);
-        System.out.println("Started test HTTP server on " + port);
+        int port = server.run(0);
+        facade = new ServerFacade("http://localhost:" + port);
+        System.out.println("Started test HTTP server on port " + port);
     }
 
     @AfterAll
-    static void stopServer() {
+    void stopServer() {
         server.stop();
     }
 
-
-    @Test
-    public void sampleTest() {
-        Assertions.assertTrue(true);
+    @BeforeEach
+    void clearDatabase() throws DataAccessException {
+        authDAO = new MySQLAuthDAO();
+        gameDAO = new MySQLGameDAO();
+        userDAO = new MySQLUserDAO();
+        authDAO.clear();
+        userDAO.clear();
+        gameDAO.clear();
     }
 
+    @Test
+    void register_validUser_succeeds() {
+        UserData user = new UserData("test_user", "pass", "test.com");
+        assertDoesNotThrow(() -> facade.register(user));
+    }
+
+    @Test
+    void register_duplicateUser_fails() {
+        UserData user = new UserData("test_user", "pass", "test.com");
+        assertDoesNotThrow(() -> facade.register(user));
+        ResultException e = assertThrows(ResultException.class, () -> facade.register(user));
+        assertEquals(403, e.statusCode());
+    }
 }
