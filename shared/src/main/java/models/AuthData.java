0205cc99b307144d4b3cdc1d1990c23aa648140e
Adriano Demartin <adriano.demartinez@gmail.com>
2025-10-11 11:35:23 -0600

    created the record classes in shared

---
+++ shared/src/main/java/models/AuthData.java
@@ -0,0 +1,7 @@
+package models;
+
+public record AuthData(
+        String authToken,
+        String username
+){
+}
---
+++ shared/src/main/java/models/UserData.java
@@ -0,0 +1,9 @@
+package models;
+
+public record UserData(
+
+        String username,
+        String password,
+        String email
+) {
+}
