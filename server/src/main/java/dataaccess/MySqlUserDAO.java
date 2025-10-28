a4fba6a3e9058f1f97fd0acae4490e6f34e64af9
Adriano Demartin <adriano.demartinez@gmail.com>
2025-10-27 18:32:20 -0600

    implemented MySqlUserDAO methods

--- server/src/main/java/dataaccess/MySqlUserDAO.java
+++ server/src/main/java/dataaccess/MySqlUserDAO.java
@@ -1,10 +1,9 @@
 package dataaccess;
 
 import models.UserData;
+import org.mindrot.jbcrypt.BCrypt;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class MySqlUserDAO implements UserDAO{
 
@@ -14,18 +13,68 @@ public class MySqlUserDAO implements UserDAO{
 
     @Override
     public void createUser(UserData u) throws DataAccessException {
+        String sql = "INSERT INTO users (username, password, email) VALUES (?, ?, ?)";
+        String hashedPassword = BCrypt.hashpw(u.password(), BCrypt.gensalt());
+        updateData(sql, u.username(), hashedPassword, u.email());
+
 
     }
 
     @Override
     public UserData getUser(String username) throws DataAccessException {
+        try(Connection conn = DatabaseManager.getConnection()) {
+            String sql = "SELECT username, password, email FROM users WHERE username = ?";
+
+            try(PreparedStatement ps = conn.prepareStatement(sql)) {
+                ps.setString(1, username);
+                try(ResultSet rs = ps.executeQuery()) {
+
+                    if (rs.next()) {
+                        String password = rs.getString("password");
+                        String email = rs.getString("email");
+                        return new UserData(username, password, email);
+                    }
+                }
+            }
+        }
+        catch (SQLException error) {
+            throw new DataAccessException(error.getMessage());
+        }
         return null;
     }
 
     @Override
     public void clear() throws DataAccessException {
+        String sql = "TRUNCATE TABLE users";
+        updateData(sql);
 
     }
+
+
+    private int updateData(String statement, Object... params) throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection()) {
+            try (PreparedStatement ps = conn.prepareStatement(statement)) {
+                for (int i = 0; i < params.length; i++) {
+                    Object param = params[i];
+
+                    if (param instanceof String p) {
+                        ps.setString(i + 1, p);
+
+                    }
+                    else if (param == null) {
+                        ps.setNull(i + 1, Types.NULL);
+                    }
+                }
+                ps.executeUpdate();
+                return 0;
+            }
+        }
+        catch (SQLException error) {
+            throw new DataAccessException(error.getMessage());
+        }
+    }
+
+
 
 
     private final String[] createStatements = {
