c465acb38d7e1da54bb02eb839c9bd0f16eae4b4
Adriano Demartin <adriano.demartinez@gmail.com>
2025-10-30 19:08:20 -0600

    implemented MysqlAuthDao methods

--- server/src/main/java/dataaccess/MySqlAuthDAO.java
+++ server/src/main/java/dataaccess/MySqlAuthDAO.java
@@ -2,10 +2,7 @@ package dataaccess;
 
 import models.AuthData;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.Types;
+import java.sql.*;
 
 public class MySqlAuthDAO implements AuthDAO{
     public MySqlAuthDAO () throws DataAccessException {
@@ -15,21 +12,45 @@ public class MySqlAuthDAO implements AuthDAO{
 
     @Override
     public void createAuth(AuthData auth) throws DataAccessException {
+        String sql = "INSERT INTO auth (authToken, username) VALUES (?, ?)";
+        updateData(sql, auth.authToken(), auth.username());
 
     }
 
     @Override
     public AuthData getAuth(String authToken) throws DataAccessException {
-        return null;
+        try (Connection conn = DatabaseManager.getConnection()) {
+            String sql = "SELECT authToken, username FROM auth WHERE authToken=?";
+
+            try (PreparedStatement ps = conn.prepareStatement(sql)) {
+                ps.setString(1, authToken);
+                try (ResultSet rs = ps.executeQuery()) {
+
+                    if (rs.next()) {
+                        String username = rs.getString("username");
+                        return new AuthData(authToken, username);
+                    }
+                }
+            }
+
+        }
+
+        catch (SQLException error) {
+            throw new DataAccessException(error.getMessage());
+        }return null;
     }
 
     @Override
     public void deleteAuth(String authToken) throws DataAccessException {
+        String sql = "DELETE FROM auth WHERE authToken=?";
+        updateData(sql, authToken);
 
     }
 
     @Override
     public void clear() throws DataAccessException {
+        String sql = "TRUNCATE TABLE auth";
+        updateData(sql);
 
     }
 
