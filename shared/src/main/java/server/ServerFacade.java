3cee6d9382be2ad249a45e1cb1827e81e57a5d02
Adriano Demartin <adriano.demartinez@gmail.com>
2025-11-05 20:55:18 -0700

    added http methods to ServerFacade

--- shared/src/main/java/server/ServerFacade.java
+++ shared/src/main/java/server/ServerFacade.java
@@ -1,6 +1,11 @@
 package server;
+import com.google.gson.Gson;
+import exception.ResponseException;
 
+import java.net.URI;
 import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class ServerFacade {
 
@@ -36,5 +41,69 @@ public class ServerFacade {
     public void listGames() {
         throw new RuntimeException("not implemented!!");
     }
+
+
+    private HttpRequest buildRequest(String method, String path, Object body) {
+        var request = HttpRequest.newBuilder().uri(URI.create(serverURL + path)).method(method, makeRequestBody(body));
+
+        if (body != null) {
+            request.header("Content-Type", "application/json");
+        }
+        return request.build();
+
+
+
+    }
+
+
+    private HttpRequest.BodyPublisher makeRequestBody(Object request) {
+        if (request != null) {
+            return HttpRequest.BodyPublishers.ofString(new Gson().toJson(request));
+        }
+        else {
+            return HttpRequest.BodyPublishers.noBody();
+        }
+    }
+
+
+    private HttpResponse<String> sendRequest(HttpRequest request) throws ResponseException {
+        try {
+            return client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        }
+        catch (Exception exep){
+            throw new ResponseException(ResponseException.Code.ServerError, exep.getMessage());
+
+
+        }
+    }
+
+    private <T> T handleResponse(HttpResponse<String> response, Class<T> responseClass) throws ResponseException {
+        var status = response.statusCode();
+
+
+        if (!isSuccessful(status)) {
+            var body = response.body();
+
+            if (body != null) {
+                throw ResponseException.fromJSON(body);
+            }
+
+            throw new ResponseException(ResponseException.fromHttpStatusCode(status), "other failure: " + status);
+        }
+
+
+        if (responseClass != null) {
+            return new Gson().fromJson(response.body(), responseClass);
+        }
+
+        return null;
+    }
+
+
+    private boolean isSuccessful(int status) {
+        return status / 100 == 2;
+    }
+
 
 }
