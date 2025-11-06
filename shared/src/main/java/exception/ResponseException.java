ddd31067837677d995ada949b82912950c6ef480
Adriano Demartin <adriano.demartinez@gmail.com>
2025-11-05 19:52:50 -0700

    created simple ResponseException

---
+++ shared/src/main/java/exception/ResponseException.java
@@ -0,0 +1,37 @@
+package exception;
+
+public class ResponseException extends Exception {
+
+    public enum Code {
+        ClientError,
+        ServerError
+    }
+
+    private final Code code;
+
+    public ResponseException(Code code, String message) {
+
+        super(message);
+        this.code = code;
+    }
+
+
+    public Code getCode() {
+        return code;
+    }
+
+
+    public static ResponseException fromJSON(String json) {
+        return new ResponseException(Code.ServerError, json);
+    }
+
+
+
+    public static Code fromHttpStatusCode(int statusCode) {
+        if (statusCode >= 400 && statusCode < 500) {
+            return Code.ClientError;
+        }
+
+        return Code.ServerError;
+    }
+}
