d9e9005f09ab84e6508177fabfd82f1990782f29
Adriano Demartin <adriano.demartinez@gmail.com>
2025-11-10 09:28:40 -0700

    implemented register, login, and logout inside ChessClient

--- client/src/main/java/client/ChessClient.java
+++ client/src/main/java/client/ChessClient.java
@@ -1,14 +1,25 @@
 package client;
 
+import exception.ResponseException;
+import results.LoginResult;
+import results.RegisterResult;
+import server.ServerFacade;
+
 import java.util.Arrays;
 import java.util.Scanner;
+
+import static ui.EscapeSequences.*;
 
 public class ChessClient {
 
     private State state = State.LOGGED_OUT;
+    private final ServerFacade server;
+    private String authToken;
+    private String username;
 
 
-    public ChessClient() {
+    public ChessClient(String serverURL) {
+        this.server = new ServerFacade(serverURL);
     }
 
     public void run() {
@@ -70,16 +81,72 @@ public class ChessClient {
             String[] params = Arrays.copyOfRange(tokens, 1, tokens.length);
             return switch (command) {
                 case "help" -> help();
+                case "register" -> register(params);
+                case "login" -> login(params);
+                case "logout" -> logout();
                 case "quit" -> "quit";
-                default -> help();
+                default -> throw new IllegalStateException("Unexpected input: " + command +"\n");
             };
 
-        }
-
-        catch (Exception exep) {
+        } catch (Exception exep) {
             return exep.getMessage();
         }
     }
+
+
+    private String register(String... params) throws ResponseException {
+        if (state != State.LOGGED_OUT) {
+            return SET_TEXT_COLOR_RED + "You are logged in. Logout to register \n" + RESET_TEXT_COLOR;
+
+        }
+        if (params.length < 3) {
+            return SET_TEXT_COLOR_RED + "Wrong input! Type: register <USERNAME> <PASSWORD> <EMAIL> \n" + RESET_TEXT_COLOR;
+        }
+        RegisterResult result = server.register(params[0], params[1], params[2]);
+        this.authToken = result.authToken();
+        this.username = result.username();
+        this.state = State.LOGGED_IN;
+        return SET_TEXT_COLOR_GREEN + "Registered and logged in as " + username +"!\n" + RESET_TEXT_COLOR;
+    }
+
+
+    private String login(String... params) throws ResponseException {
+        if (state == State.LOGGED_IN) {
+            return SET_TEXT_COLOR_RED + "You're already logged in!\n"+ RESET_TEXT_COLOR;
+
+        }
+
+        if (params.length < 1) {
+            return SET_TEXT_COLOR_RED + "Wrong input! Type: login <USERNAME> <PASSWORD>\n"+ RESET_TEXT_COLOR;
+
+        }
+
+        LoginResult result = server.login(params[0], params[1]);
+        this.authToken = result.authToken();
+        this.username = result.username();
+        this.state = State.LOGGED_IN;
+        return SET_TEXT_COLOR_GREEN + "Loggined in as " + username + "!\n" + RESET_TEXT_COLOR;
+
+
+
+    }
+
+
+    private String logout() throws ResponseException {
+        if (state == State.LOGGED_OUT) {
+            return SET_TEXT_COLOR_RED + "You're not logged in!\n"+ RESET_TEXT_COLOR;
+
+        }
+
+        server.logout();
+        this.authToken = null;
+        this.username = null;
+        this.state = State.LOGGED_OUT;
+
+
+        return SET_TEXT_COLOR_GREEN + "You logged out!" + RESET_TEXT_COLOR;
+    }
+
 
 
 
