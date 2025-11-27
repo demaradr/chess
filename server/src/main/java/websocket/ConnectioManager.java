b0464669b1f858a0342e8581ade5e0440f04e664
Adriano Demartin <adriano.demartinez@gmail.com>
2025-11-27 08:54:18 -0700

    started Connection manager and websocket handler

---
+++ server/src/main/java/websocket/ConnectioManager.java
@@ -0,0 +1,37 @@
+package websocket;
+
+import chess.ChessGame;
+import io.javalin.websocket.WsContext;
+import org.eclipse.jetty.websocket.api.Session;
+
+import javax.management.Notification;
+import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConnectioManager {
+
+    public final ConcurrentHashMap<String, Connection> connections = new ConcurrentHashMap<>();
+
+
+    public void add(Connection connection) {
+        connections.put(connection.sessionID(), connection);
+    }
+
+    public void remove(String sessionID) {
+        connections.remove(sessionID);
+    }
+
+    public void broadcast(Session excludeSession, Notification notification ) throws IOException {
+        String message = notification.toString();
+        for (Session c : connections.values()) {
+            if (c.isOpen()) {
+                if (!c.equals(excludeSession)) {
+                    c.getRemote().sendString(message);
+                }
+            }
+        }
+    }
+
+    public record Connection(String sessionID, WsContext context, int gameID, String user, ChessGame.TeamColor color) {
+    }
+}
